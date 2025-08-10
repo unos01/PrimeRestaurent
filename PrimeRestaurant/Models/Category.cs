5da7d3fa7e6f7e367497f48e31ace4e1ddcbc77e
Sonu Sharma <sonusharma98199@gmail.com>
2025-08-11 02:31:33 +0530

    project added

---
+++ PrimeRestaurant/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace PrimeRestaurant.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
